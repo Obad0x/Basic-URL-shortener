9ccecd940b0fb0c6109d2c92d442b15da9d8bd0d
SIta Dabo <94798285+Obad0x@users.noreply.github.com>
2023-12-26 23:34:13 +0100

    added the function to shorten url in the db

--- server/Routes/main.js
+++ server/Routes/main.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Url = require('../Models/URL')
+
+
+
+
 
 router.get('/', (req, res) => res.render('index'))
+
+router.post('/shorten-url', async (req, res)=>{
+      try {
+        const url = new Url({
+            LongUrl: req.body.url
+        })
+            await Url.create(url);
+            res.redirect('/')
+      } catch (error) {
+        console.log(error)
+        
+      }
+    
+    
+    
+})
 
 module.exports = router;
