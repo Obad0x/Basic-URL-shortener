c25f0a4ddbf4bec229d907c6240c1b196a4a5f15
SIta Dabo <94798285+Obad0x@users.noreply.github.com>
2023-12-27 14:47:38 +0100

    added the redirecting feature

--- server/Routes/main.js
+++ server/Routes/main.js
@@ -4,9 +4,16 @@ const Url = require('../Models/URL')
 
 
 
+/* GET 
+    /- Home Page */
+
+router.get('/', (req, res) => res.render('index'));
 
 
-router.get('/', (req, res) => res.render('index'))
+
+/* POST 
+
+/shorten-url- url shortener*/
 
 router.post('/shorten-url', async (req, res)=>{
       try {
@@ -14,7 +21,8 @@ router.post('/shorten-url', async (req, res)=>{
             LongUrl: req.body.url
         })
             await Url.create(url);
-            res.redirect('/')
+            res.send(url)
+            
       } catch (error) {
         console.log(error)
         
@@ -23,5 +31,22 @@ router.post('/shorten-url', async (req, res)=>{
     
     
 })
+
+
+/* POST
+ : ID - get the link */
+
+ router.get('/:ShortUrl', async(req, res)=>{
+
+      const id = req.params.ShortUrl;
+
+      const data = await Url.findOne({
+        ShortUrl : id
+      })
+      res.redirect(data.LongUrl)
+ })
+
+
+
 
 module.exports = router;
